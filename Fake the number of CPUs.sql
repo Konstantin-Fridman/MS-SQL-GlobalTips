defcf2abb3f9acec659d265ac7fe53c8cbc6f9a5
Konstantin-Fridman <Konstantin-Fridman@users.noreply.github.com>
2014-06-12 16:46:18 +0300

    Create Fake the number of CPUs.sql

---
+++ Fake the number of CPUs.sql
@@ -0,0 +1,13 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\
+-- Fake the number of CPUs ( for an execution plan only ) 
+\*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+DBCC OPTIMIZER_WHATIF(1, 64) --! 
+
+SELECT top 
+	   actid,
+       tranid,
+       val,
+       Ntile(100)         OVER(           PARTITION BY actid           ORDER BY val) AS rownum
+FROM   dbo.Transactions 
+OPTION(RECOMPILE) --!
+
